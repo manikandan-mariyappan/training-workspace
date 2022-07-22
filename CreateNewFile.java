4c216365e5f130d74053f25e59bdd3dd256421c1
manikandan <manikandantharsith@gmail.com>
2022-07-22 16:19:21 +0530

    File creation done

---
+++ CreateNewFile.java
@@ -0,0 +1,19 @@
+
+  
+class CreateNewFile {
+    public static void main(String[] args)
+    {
+  
+        // File name specified
+        
+          System.out.println("File Created!");
+    }
+}
+
+
+  
+    
+   
+     
+
+     

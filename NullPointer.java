296e4a7a196af65f1a959877e2d669a8eb2054be
manikandan <manikandantharsith@gmail.com>
2022-07-30 17:36:19 +0530

    SUccess

---
+++ NullPointer.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class NullPointer {
+
+	public static void main(String[] args) {
+		
+		System.out.println("enter a number");
+		Scanner scan = new Scanner(System.in);
+		System.out.println(scan.nextInt());
+		
+		
+		
+
+	}
+
+}
